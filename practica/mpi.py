e67ecbfbaa53d94fd2f498eeb7c0e907f49114c9
Diego Linares <diegolinares@MacBook-Pro-de-Diego.local>
2019-06-26 08:28:10 -0500

    Corrected some issues with the graphs. Not hardcoded anymore.

--- practica/mpi.py
+++ practica/mpi.py
@@ -3,6 +3,8 @@ import sys
 import matplotlib.pyplot as plt
 
 def main():
+    
+    cores = int(input("Insert number of cores: "))
     clean_comand = 'make clean'
     compile_comand = 'make'
     os.system(clean_comand)
@@ -13,7 +15,7 @@ def main():
     file_name = sys.argv[1]
     print("File name: ", file_name)
 
-    for i in range(0, 3):
+    for i in range(0, cores):
         out_time = os.popen('mpirun -np ' + str(2**i) + ' gaussian '+ file_name).read()
         test_cores.append(2**i)
         test_time.append(float(out_time[:-2]))
@@ -23,8 +25,8 @@ def main():
     plt.xlabel("Number of threads / AU", fontsize=10)
     plt.ylabel("Time / s", fontsize=10)
     for y in range(0, int(max(test_time))):
-      plt.plot(range(1,2**(3-1)), [y] * len(range(1,2**(3-1))), "--", lw=0.5, color="black", alpha=0.3)
-    plt.xticks(range(1,2**(3-1)+1), fontsize=10)  
+      plt.plot(range(1,2**(cores-1)+1), [y] * len(range(1,2**(cores-1)+1)), "--", lw=0.5, color="black", alpha=0.3)
+    plt.xticks(range(1,2**(cores-1)+1), fontsize=10)  
     plt.yticks(fontsize=10)
     
     fig, ax = plt.subplots()
